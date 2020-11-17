8a9fbb267a4194bed9124886a1f8354f7ae5258d
pangnem <sprnd645@gmail.com>
2020-11-18 08:21:41 +0900

    feat: auth rest api resources

---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,46 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
+
+enum memberType {
+  SITTER = 'SITTER',
+  PARENT = 'PARENT',
+}
+
+export default class CreateUserDto {
+  @IsNotEmpty()
+  id: number;
+
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  birth: number;
+
+  @IsNotEmpty()
+  gender: string;
+
+  @IsNotEmpty()
+  user_id: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsEnum(memberType)
+  member_type: string;
+
+  @IsOptional()
+  careable_baby_age: number;
+
+  @IsOptional()
+  self_introduction: string;
+
+  @IsOptional()
+  desired_baby_age: number;
+
+  @IsOptional()
+  request_infomation: string;
+}
